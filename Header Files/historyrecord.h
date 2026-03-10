15e89e43883ce1a39b300cbf50188dfbc965916b
Xander Xiao <138387398+ckckh2023@users.noreply.github.com>
2026-03-10 16:40:34 +0800

    Add files via upload

--- Header Files/historyrecord.h
+++ Header Files/historyrecord.h
@@ -3,9 +3,7 @@
 
 #include <QObject>
 #include <QSqlDatabase>
-#include <QSqlError>
-#include <QDebug>
-#include <QSqlQuery>
+#include <QString>
 
 class HistoryRecord : public QObject{
     Q_OBJECT
@@ -14,10 +12,12 @@ public:
     explicit HistoryRecord(QObject *parent = nullptr);
     ~HistoryRecord();
 
+    void generateThumbail(const QString &ImagePath, const QString &CurrentTime);
     void addTrashTables(const QString &path,const QString &result);
     void addFaceTables(const QString &path,const QString &result);
     void openDb();
     void closeDb();
+    Q_INVOKABLE QVariantList getAllRecords();
 
 signals:
     void messageSent(const QString &msg);
