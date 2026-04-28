7dca974ad615c4bc7cea5c5c4070bf2b685540cb
cloudy-rhh <3551570732@qq.com>
2026-04-28 18:51:33 +0800

    更改文件间距

--- include/HistoryRecord.h
+++ include/HistoryRecord.h
@@ -28,6 +28,7 @@ signals:
     void messageSentInfo(const QString &info);
     void messageSentError(const QString &error);
     void messageSentWarn(const QString &warn);
+
 private:
     void openDb();
     void closeDb();
